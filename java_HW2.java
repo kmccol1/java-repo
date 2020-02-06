ce04146fcf2bcca4977e555475ebc0baf9a0a11a
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-02-05 21:04:18 -0600

    Add files via upload

---
+++ java_HW2.java
@@ -0,0 +1,34 @@
+public class Rectangle
+{
+	private int length;
+	private int width;
+	
+	Rectangle ( )
+	{
+		length = width = 0;
+	}
+	
+	public int getLength ( )
+	{
+		return this.length;
+	}
+	
+	public	int getWidth ( )
+	{
+		return this.width;
+	}
+		
+	public	void setLength ( int len )
+	{
+		this.length = len;
+	}
+	
+	public	void setWidth ( int width )
+	{
+		this.width = width;
+	}
+
+}
+
+
+		
