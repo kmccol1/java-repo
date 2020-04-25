95692bff25b4d1c9114797cbb3e196a537716e96
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-04-24 22:04:37 -0500

    Update TipTaxTotalApp.java

--- TipTaxTotalApp.java
+++ TipTaxTotalApp.java
@@ -1,27 +1,3 @@
-//****************************************************************************************************
-//
-//              File:                                   TipTaxTotalApp.java
-//		
-//              Student:                                Kyle McColgan
-//
-//              Assignment:                             Program  # 10
-//
-//              Course Name:                            Java programming I
-// 
-//              Course Number:                          COSC 2050 - 01
-//
-//              Due:                                    April 15th, 2020
-//
-//
-//              This file contains the TipTaxTotal JavaFX driver class.
-//
-//              Other files required:
-//                      1. TipTaxTotalController.java - function implementations.
-//                      2. FXMLDocument.fxml - Required file to build the JavaFX GUI project
-//                      3. Validator.java - User-input validator utility class.
-//		 
-//		
-//****************************************************************************************************
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
