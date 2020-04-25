2ce757f9136c542ba9acb9c1c078cabe8732a3d3
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-04-24 22:05:05 -0500

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -1,27 +1,3 @@
-//****************************************************************************************************
-//
-//              File:                                   Validator.java
-//		
-//              Student:                                Kyle McColgan
-//
-//              Assignment:                             Program  # 10
-//
-//              Course Name:                            Java programming I
-// 
-//              Course Number:                          COSC 2050 - 01
-//
-//              Due:                                    April 15th, 2020
-//
-//
-//              This file contains the user-input Validator class.
-//
-//              Other files required:
-//                      1. TipTaxTotalApp.java - Application driver class.
-//                      2. FXMLDocument.fxml - Required file to build the JavaFX GUI project
-//                      3. TipTaxTotalController.java - function implementations.
-//		 
-//		
-//****************************************************************************************************
 
 public class Validator 
 {
