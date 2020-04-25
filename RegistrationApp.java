dc02d7de7b126f7cda52ab924f49906e63cdf334
kmccol1 <60723358+kmccol1@users.noreply.github.com>
2020-04-24 22:01:13 -0500

    Update RegistrationApp.java

--- RegistrationApp.java
+++ RegistrationApp.java
@@ -1,27 +1,3 @@
-//****************************************************************************************************
-//
-//              File:                                   RegistrationApp.java
-//		
-//              Student:                                Kyle McColgan
-//
-//              Assignment:                             Program  # 9
-//
-//              Course Name:                            Java programming I
-// 
-//              Course Number:                          COSC 2050 - 01
-//
-//              Due:                                    April 8th, 2020
-//
-//
-//              This file contains the Registration driver class.
-//
-//              Other files required:
-//                      1. RegistrationController.java - Registration function implementations.
-//                      2. FXMLDocument.fxml - Required file to build the JavaFX GUI project
-//                      3. Validator.java - User-input validator utility class.
-//		 
-//		
-//****************************************************************************************************
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
